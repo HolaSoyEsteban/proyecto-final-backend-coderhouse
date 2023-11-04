84dfe16d66b56bff5099900e26119f732ed60993
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-04 14:40:40 -0300

    Deployando el proyecto en Vercel, intento 19

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import app from "./src/app";
-// import config from './src/config/config.js';
+import config from './src/config/config.js';
 
-// const port = 8080
-// app.listen(port)
+const port = 8080
+app.listen(port)
 
-// console.log(`Listen on port ${port}`)
+console.log(`Listen on port ${port}`)
 
 export default app;
