794826a34555ebedf32466c7cd4476d75e9aa395
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-04 15:10:27 -0300

    Deployando el proyecto en Vercel, intento 21

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 import app from "./src/app";
-import config from './src/config/config.js';
-
-const port = 'https://proyecto-final-backend-coderhouse.vercel.app/';
-app.listen(port)
-
-console.log(`Listen on port ${port}`)
 
 export default app;
